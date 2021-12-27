4be0b47d86ffed4b9dca219327374ae09857148b
goflutterjava <goflutterjava@gmail.com>
2021-12-27 08:54:25 +0800

    bump springboot to 2.6.1

--- hadoop-mate/src/main/java/com/github/shoothzj/hadoop/mate/service/LifecycleService.java
+++ hadoop-mate/src/main/java/com/github/shoothzj/hadoop/mate/service/LifecycleService.java
@@ -3,11 +3,11 @@ package com.github.shoothzj.hadoop.mate.service;
 import com.github.shoothzj.hadoop.mate.config.HadoopConfig;
 import com.github.shoothzj.hadoop.mate.constant.PathConst;
 import com.github.shoothzj.javatool.util.ShellUtil;
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
 
 /**
  * @author hezhangjian
@@ -37,13 +37,13 @@ public class LifecycleService {
     public void startHadoop() throws Exception {
         switch (hadoopConfig.deployType) {
             case DATA:
-                ShellUtil.executeCmd("bash -x " + PathConst.HADOOP_START_DATA_SCRIPT);
+                ShellUtil.executeScript(PathConst.HADOOP_START_DATA_SCRIPT);
                 break;
             case NAME:
-                ShellUtil.executeCmd("bash -x " + PathConst.HADOOP_START_NAME_SCRIPT);
+                ShellUtil.executeScript(PathConst.HADOOP_START_NAME_SCRIPT);
                 break;
             case PSEUDO:
-                ShellUtil.executeCmd("bash -x " + PathConst.HADOOP_START_PSEUDO_SCRIPT);
+                ShellUtil.executeScript(PathConst.HADOOP_START_PSEUDO_SCRIPT);
                 break;
             default:
                 break;
