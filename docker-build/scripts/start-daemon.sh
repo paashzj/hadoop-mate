c798ea00d70d897d2114f10c921822e5169c43f9
lovehzj <1922919664@qq.com>
2022-01-13 23:16:03 +0800

    Release docker image binary

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -4,6 +4,6 @@
 
 sleep 5
 
-mkdir $HADOOP_HOME/logs
+mkdir -p $HADOOP_HOME/logs
 
 java -Xmx128M -Xms128M -XX:MaxDirectMemorySize=256M -jar $HADOOP_HOME/mate/hadoop-mate.jar >>$HADOOP_HOME/logs/hadoop_mate.stdout.log 2>>$HADOOP_HOME/logs/hadoop_mate.stderr.log
